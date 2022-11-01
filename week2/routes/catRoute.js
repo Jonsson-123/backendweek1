eeba8b8f5966c214ee08f7b40d68c826ec2d3464
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2022-11-01 11:55:05 +0200

    week2

---
+++ week2/routes/catRoute.js
@@ -0,0 +1,24 @@
+'use strict';
+const express = require('express');
+const {cat_list_get, get_cat, cat_get} = require('../controllers/catController');
+const router = express.Router();
+
+
+
+router.get('/', cat_list_get );
+
+router.get('/:id', cat_get);
+
+router.post('/', (req, res) => {
+  res.send('With this endpoint you can add cats.');
+});
+
+router.put('/', (req, res) => {
+  res.send('With this endpoint you can edit cats.');
+});
+
+router.delete('/', (req, res) => {
+  res.send('With this endpoint you can delete cats.');
+});
+
+module.exports = router;
