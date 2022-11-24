2c5b1f3239a6c5694eceac548f43e2cc31c82613
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2022-11-24 11:33:56 +0200

    https

--- week2/app.js
+++ week2/app.js
@@ -10,22 +10,14 @@ const passport = require('./utils/pass')
 const app = express();
 const port = 3000;
 
-app.enable('trust proxy');
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-app.use ((req, res, next) => {
-    if (req.secure) {
-        // request was via https, so do no special handling
-        next();
-    } else {
-        // if express app run under proxy with sub path URL
-        // e.g. http://www.myserver.com/app/
-        // then, in your .env, set PROXY_PASS=/app
-        // Adapt to your proxy settings!
-        const proxypath = process.env.PROXY_PASS || ''
-        // request was via http, so redirect to https
-        res.redirect(301, `https://${req.headers.host}${proxypath}${req.url}`);
-    }
-});
+if (process.env.NODE_ENV === 'production') {
+    require('./utils/production')(app, process.env.HTTP_PORT || 3000, process.env.HTTPS_PORT || 8000);
+} else {
+    require('./utils/localhost')(app, process.env.HTTP_PORT || 3000);
+}
+
 
 app.use(cors());
 app.use(express.json()) // for parsing application/json
@@ -49,4 +41,3 @@ app.use((err, req, res, next) => {
     res.status(err.status || 500).json({message: err.message || 'Internal server error'})
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
